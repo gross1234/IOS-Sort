b04682d8f8a99f08fc0fc00676b4c4a539a2adaa
gross1234 <ross.gabem@gmail.com>
2025-10-04 14:40:16 -0500

    Initial Commit

---
+++ IOS Sort/IOS_SortApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_SortApp.swift
+//  IOS Sort
+//
+//  Created by Gabe Ross on 10/4/25.
+//
+
+import SwiftUI
+
+@main
+struct IOS_SortApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IOS SortTests/IOS_SortTests.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_SortTests.swift
+//  IOS SortTests
+//
+//  Created by Gabe Ross on 10/4/25.
+//
+
+import Testing
+@testable import IOS_Sort
+
+struct IOS_SortTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
